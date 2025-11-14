7c7f62b29824c2dabb807f2f39b496bf47721610
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2025-11-14 11:24:00 +0800

    Create masonry merge structure.

--- commands/masonry.js
+++ commands/masonry.js
@@ -1,6 +1,8 @@
 import { Command } from 'commander';
-import { addSharedOptions } from '../lib/helpers/utils.js';
+import { addSharedOptions, writeImage } from '../lib/helpers/utils.js';
 import { validateSharedOptions, validateMasonryOptions } from '../lib/helpers/validations.js';
+import { loadImages } from '../lib/helpers/loadImages.js';
+import { masonryMerge } from '../lib/merges/masonry-merge/index.js';
 
 const masonryCommand = new Command('masonry');
 
@@ -14,15 +16,32 @@ masonryCommand
   .option('--ha, --h-align <left|center|right|justified>', 'Horizontal alignment of the grid (for horizontal orientations)', null)
   .option('--va, --v-align <top|middle|bottom|justified>', 'Vertical alignment of the grid (for vertical orientations)', null)
   .action((files, opts) => {
-    const params = { files, ...opts };
-    const sharedOptions = validateSharedOptions(params);
-    const commandOptions = validateMasonryOptions(params);
-    const validatedParams = { ...sharedOptions, ...commandOptions };
-
-    console.log(validatedParams);
-
-    // Run masonryMerge
+    main(files, opts);
   });
+
+const main = async (files, opts) => {
+  // Collect and validate parameters
+  const validatedParams = getValidatedParams(files, opts);
+  console.log(validatedParams);
+
+  // Load images, create grid, and write grid on disk
+  generateAndSaveGrid(validatedParams);
+
+  // Output success message
+};
+
+const getValidatedParams = (files, opts) => {
+  const params = { files, ...opts };
+  const sharedOptions = validateSharedOptions(params);
+  const commandOptions = validateMasonryOptions(params);
+  return { ...sharedOptions, ...commandOptions };
+};
+
+const generateAndSaveGrid = async (validatedParams) => {
+  const images = await loadImages(validatedParams);
+  const grid = masonryMerge(images, validatedParams);
+  // writeImage(grid, validatedParams.output);
+};
 
 addSharedOptions(masonryCommand);
 export default masonryCommand;
