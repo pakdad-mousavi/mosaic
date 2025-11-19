8db9ad27fe8e3fffc95cc8443b268e32778cba95
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2025-11-19 17:23:58 +0800

    Add aspect merge command to the project.

---
+++ commands/aspect.js
@@ -0,0 +1,42 @@
+import { Command } from 'commander';
+import { addSharedOptions, getValidatedParams, handleError, writeImage } from '../lib/helpers/utils.js';
+import { validateAspectOptions } from '../lib/helpers/validations.js';
+import { loadImages } from '../lib/helpers/loadImages.js';
+import { aspectMerge } from '../lib/merges/aspect-merge/index.js';
+
+const aspectCommand = new Command('aspect');
+
+aspectCommand
+  .description('Same aspect ratio, but not necessarily 1:1')
+  .option('--ar, --aspect-ratio <width/height|number>', 'The aspect ratio of all the images (examples: 16/9, 4:3, 1.777)', null)
+  .option('-w, --image-width <px>', 'The width of each image, defaults to the smallest image', null)
+  .option('-c, --columns <n>', 'The number of columns', 4)
+  .option('--caption', 'Whether to caption each image', false)
+  .option('--caption-color <hex|transparent>', 'Image caption color', '#000000')
+  .action(async (files, opts) => {
+    await main(files, opts);
+  });
+
+const main = async (files, opts) => {
+  // Collect and validate parameters
+  try {
+    const validatedParams = getValidatedParams(files, opts, validateAspectOptions);
+    console.log(validatedParams);
+
+    // Load images, create grid, and write grid on disk
+    await generateAndSaveGrid(validatedParams);
+
+    // Output success message
+  } catch (e) {
+    handleError(e);
+  }
+};
+
+const generateAndSaveGrid = async (validatedParams) => {
+  const images = await loadImages(validatedParams);
+  const grid = await aspectMerge(images, validatedParams);
+  writeImage(grid, validatedParams.output);
+};
+
+addSharedOptions(aspectCommand);
+export default aspectCommand;
--- commands/merge.js
+++ commands/merge.js
@@ -1,10 +1,12 @@
 import { Command } from 'commander';
 import squareCommand from './square.js';
 import masonryCommand from './masonry.js';
+import aspectCommand from './aspect.js';
 
 const mergeCommand = new Command('merge').description('Merge images into a grid layout.');
 
 mergeCommand.addCommand(squareCommand);
 mergeCommand.addCommand(masonryCommand);
+mergeCommand.addCommand(aspectCommand);
 
 export default mergeCommand;
