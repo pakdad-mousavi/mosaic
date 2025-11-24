4da523ff5ea48b01ffac807b2674a2bd388a385d
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2025-11-24 12:50:17 +0800

    Remove square command from merge and update grid options validation.

--- commands/merge.js
+++ commands/merge.js
@@ -1,11 +1,9 @@
 import { Command } from 'commander';
-import squareCommand from './square.js';
 import masonryCommand from './masonry.js';
 import gridCommand from './grid.js';
 
 const mergeCommand = new Command('merge').description('Merge images into a grid layout.');
 
-mergeCommand.addCommand(squareCommand);
 mergeCommand.addCommand(masonryCommand);
 mergeCommand.addCommand(gridCommand);
 
