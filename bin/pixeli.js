cfb3c8178c5aa3be88d1da953bb8486cb9175546
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2025-12-03 12:57:05 +0800

    Remove dedupe command from CLI.

--- bin/pixeli.js
+++ bin/pixeli.js
@@ -2,7 +2,6 @@
 
 import { Command } from 'commander';
 import mergeCommand from '../commands/merge/index.js';
-import dedupeCommand from '../commands/dedupe/index.js';
 import { configureCommandErrors, handleError } from '../lib/helpers/utils.js';
 
 const program = new Command();
@@ -15,7 +14,6 @@ program
 
 // Add subcommands
 program.addCommand(mergeCommand);
-program.addCommand(dedupeCommand);
 
 // Configure errors for all subcommands
 configureCommandErrors(program);
